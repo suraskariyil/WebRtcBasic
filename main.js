c462f168ffaf214d0ec1bb0bcb2bb3126f096dab
Suras Kariyil <suraskariyil@gmail.com>
2025-03-16 23:55:28 +0530

    base layer

---
+++ main.js
@@ -0,0 +1,7 @@
+let localStream;
+let remoteStream;
+let init = async () => {
+    localStream = await navigator.mediaDevices.getUserMedia({video:true,audio:false})
+    document.getElementById('user-1').srcObject = localStream
+}
+init()
