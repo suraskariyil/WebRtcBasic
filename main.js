d5d7e7a7f0fb5764d1ba42b6c70feadd4f509a5f
Suras Kariyil <suraskariyil@gmail.com>
2025-03-19 21:17:29 +0530

    peerconnection

--- main.js
+++ main.js
@@ -1,7 +1,48 @@
+let APP_ID ;""
+
 let localStream;
 let remoteStream;
+let peerConnection;
+
+const servers ={
+    iceServers :[
+        {
+            urls:["stun:stun.l.google.com:19302",
+                "stun:stun.l.google.com:5349"]
+        }
+    ]
+}
+
+
 let init = async () => {
     localStream = await navigator.mediaDevices.getUserMedia({video:true,audio:false})
     document.getElementById('user-1').srcObject = localStream
+
+    createOffer()
+}
+let createOffer = async ()=> {
+    peerConnection = new RTCPeerConnection (servers)
+    remoteStream = new MediaStream()
+    document.getElementById('user-2').srcObject = remoteStream
+
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track,localStream)
+    })
+     
+    peerConnection.ontrack = (event) => {
+        event.streams[0].getTracks().forEachn((track) => {
+            remoteStream.addTrack(track)
+        })
+    }
+
+    peerConnection.onicecandidate = async (event) => {
+        if (event.candidate){
+            console.log( 'NEW ICE CANDIDATE',event.candidate)
+        }
+    }
+
+    let offer = await peerConnection.createOffer()
+    await peerConnection.setLocalDescription(offer)
+    console.log('Offer:',offer)
 }
 init()
